84a94ab0f9d323111656ff8ef344c90161ed2d67
Shany <shantia08.azizian@gmail.com>
2025-10-22 09:19:09 +0200

    the learning tab(beta version)

---
+++ WhasubiesAppTry1/Learn/ListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListDetailView.swift
+//  WhasubiesAppTry1
+//
+//  Created by shantia azizian on 20/10/25.
+//
+
+import SwiftUI
+
+struct ListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListDetailView()
+}
