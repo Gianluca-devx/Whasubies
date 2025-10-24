7a656f398e639a28597da20c873f54455ad8ec31
Aleksandra <aleksandra.stupiec@gmail.com>
2025-10-24 02:05:59 +0200

    diary new model files

---
+++ WhasubiesAppTry1/Diary/DiaryDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryDetailView.swift
+//  WhasubiesAppTry1
+//
+//  Created by Aleksandra Stupiec on 23/10/25.
+//
+
+import SwiftUI
+
+struct DiaryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiaryDetailView()
+}
---
+++ WhasubiesAppTry1/Diary/DiaryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DiaryViewModel.swift
+//  WhasubiesAppTry1
+//
+//  Created by Aleksandra Stupiec on 23/10/25.
+//
+
+import Foundation
