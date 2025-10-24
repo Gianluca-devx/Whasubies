270a04226cad7479d3e488fcba2e65ad4d3cc554
Aleksandra <aleksandra.stupiec@gmail.com>
2025-10-24 12:08:26 +0200

    FINAL VERSION DUH

---
+++ WhasubiesAppTry1/Learn/ListModel.swift
@@ -0,0 +1,16 @@
+//
+//  ListModel.swift
+//  WhasubiesAppTry1
+//
+//  Created by shantia azizian on 20/10/25.
+//
+import Foundation
+import SwiftUI
+
+struct ListModel: Identifiable{
+    var id = UUID()
+    var title: String
+    var color: Color
+    var assetname : String
+    }
+
